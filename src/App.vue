ae3bee4d42eab2de9057b4a9e3d6215188b0bcca
yumayumayuma <yumapuripuri@gmail.com>
2021-07-12 14:14:39 +0900

    html書いた

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,34 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <h1>ToDoList</h1>
+    <div class="input">
+      <span>入力欄</span><br>
+      <input class="border-line" type="text" name="ToDo" id="todo">
+      <input class="border-line" type="button" value="入力ボタン">
+    </div>
+    <div class="filter">
+      <span>フィルター</span><br>
+      <span class="tag-check">未<input type="checkbox" name="d1" id="d1"></span>
+      <span class="tag-check">済<input type="checkbox" name="d2" id="d2"></span>
+    </div>
+    <div class="list">
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  
 </script>
+
+<style scoped>
+.border-line{
+  border:solid 2px black ;
+  margin-bottom: 5px;
+}
+.tag-check{
+  margin-right: 5px
+}
+</style>
