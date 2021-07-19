8d8bba96a947fc8e9e509d84a5f9acd38987b6af
yumayumayuma <yumapuripuri@gmail.com>
2021-07-19 14:52:03 +0900

    7/19-2

--- src/Todo.vue
+++ src/Todo.vue
@@ -6,6 +6,7 @@
         v-model="current"
         v-bind:value="label.value">{{ label.label }}
     </label>
+    （{{ computedTodos.length }} 件を表示）
     <!-- ToDo テーブル -->
     <table>
       <!-- テーブルヘッダー -->
@@ -30,9 +31,29 @@
         </tr>
       </thead>
       <tbody>
+          <tr
+            v-for="item in computedTodos"
+            v-bind:key="item.id"
+            v-bind:class="{done:item.state}">
+            <th>{{ item.id }}</th>
+            <td>{{ item.comment }}</td>
+            <td class="state">
+              <!-- ★STEP10 状態変更ボタン -->
+              <button v-on:click="doChangeState(item)">
+                {{ labels[item.state] }}
+              </button>
+            </td>
+            <td class="button">
+              <!-- ★STEP10 削除ボタン -->
+              <button v-on:click.ctrl="doRemove(item)">
+                削除
+              </button>
+            </td>
+          </tr>
         <!-- [1] ここに <tr> で ToDo の要素を1行づつ繰り返し表示したい -->
       </tbody>
     </table>
+  <p>※削除ボタンはコントロールキーを押しながらクリックして下さい</p>
     <!-- 新規登録フォーム -->
     <h2>新しい作業の追加</h2>
     <form class="add-form" v-on:submit.prevent="doAdd">
@@ -46,22 +67,22 @@
 
 <script>
 
-  // var STORAGE_KEY = 'todos-vuejs-demo'
-  // var todoStorage = {
-  //   fetch: function() {
-  //     var todos = JSON.parse(
-  //       localStorage.getItem(STORAGE_KEY) || '[]'
-  //     )
-  //     todos.forEach(function(todo, index) {
-  //       todo.id = index
-  //     })
-  //     todoStorage.uid = todos.length
-  //     return todos
-  //   },
-  //   save: function(todos) {
-  //     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  //   }
-  // },
+  var STORAGE_KEY = 'todos-vuejs-demo'
+  var todoStorage = {
+    fetch: function() {
+      var todos = JSON.parse(
+        localStorage.getItem(STORAGE_KEY) || '[]'
+      )
+      todos.forEach(function(todo, index) {
+        todo.id = index
+      })
+      todoStorage.uid = todos.length
+      return todos
+    },
+    save: function(todos) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+  },
 
   var todoapp = new Vue ({
     el: '#todoapp',
