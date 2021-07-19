5b34b893ca8366bd8c88ee74827a6ced52a179f7
yumayumayuma <yumapuripuri@gmail.com>
2021-07-19 14:36:19 +0900

    7/19

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,66 @@
 <template>
-  <div id="app">
-    <h1>ToDoList</h1>
-    <div class="input">
-      <span>入力欄</span><br>
-      <input class="border-line" type="text" name="ToDo" id="todo">
-      <input class="border-line" type="button" value="入力ボタン">
-    </div>
-    <div class="filter">
-      <span>フィルター</span><br>
-      <span class="tag-check">未<input type="checkbox" name="d1" id="d1"></span>
-      <span class="tag-check">済<input type="checkbox" name="d2" id="d2"></span>
-    </div>
-    <div class="list">
-      <ul>
-        <li></li>
-      </ul>
-    </div>
+  <div id="app"><!--入力スペースとボタン(クリックするとaddTodoする)-->
+    <h1>Todo List</h1>
+    <span>作業名：</span>
+    <input class="border" v-model="task" type="text">
+    <button class="border" id="add" v-on:click="addTodo">追加</button>
+    <todo-list v-on:remove="removeTodo" v-bind:items=todos></todo-list><!--コンポーネント使用--><!--v-bind調べる。:removeの起動条件、Todolist.vueにあった-->
   </div>
 </template>
-
+ 
 <script>
-  
-</script>
-
-<style scoped>
-.border-line{
-  border:solid 2px black ;
-  margin-bottom: 5px;
+import TodoList from './components/Todolist.vue'
+ 
+export default {
+  name: 'app',
+  components: {
+    TodoList//コンポーネント使用
+  },
+  data: function() {//
+    return {
+      task: "",//v-model="task"の初期値の宣言
+      todos: [],//初期値？
+      count: 0,//変数count作成のため？初期値？
+    }
+  },
+  methods: {
+    addTodo: function() {//追加ボタンを押したとき起動する
+      if (this.task === "") {//入力内容が無い場合の処理
+        alert("作業名を入力してください");
+        return;
+      }//入力内容が有る場合
+      this.todos.push({//pushは何>配列に要素を追加する。
+        message: this.task, id: ++this.count//入力内容をmassageに代入。idにtodo項目数の数値を与えている？別コンポーネントで使用？
+      });
+      this.task = "";//追加後、入力スペースを空にする
+    },//追加ボタンを押したときの処理ここまで
+    removeTodo: function(event, index) {//削除ボタンクリック時の処理、変数eventは別コンポーネントから来ていた、何のためか不明
+      this.todos.splice(index, 1);//spliceは何>配列を操作している。変数indexの数値でクリックした削除ボタンを持つliを指定、隣の1でindexで指定した配列から1つ目まで消す
+    }
+  },
+  mounted: function() {
+    this.todos = JSON.parse(localStorage.getItem("todos")) || [];//右を左に代入
+    // this.todos = [];
+    const todos = this.todos;//todosにJSON.parse(localStorage.getItem("todos"))代入
+    this.count = this.todos.length;//todoの項目の数をcountに代入
+    window.onbeforeunload = function() {//画面更新時消えないように保存している？
+      localStorage.setItem("todos", JSON.stringify(todos));//todoの記録、保存
+    }
+  }
 }
-.tag-check{
-  margin-right: 5px
+</script>
+ 
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.border {
+  border: solid 2px black;
+  margin-right: 5px;
 }
 </style>
---
+++ src/Todo.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id = "todoapp">
+    <!-- 絞り込みラジオボタン -->
+    <label v-for="(label,key) in options" :key="key">
+      <input type="radio"
+        v-model="current"
+        v-bind:value="label.value">{{ label.label }}
+    </label>
+    <!-- ToDo テーブル -->
+    <table>
+      <!-- テーブルヘッダー -->
+      <thead>
+        <!-- ここに <tr> で ToDo の要素を1行づつ繰り返し表示 -->
+        <tr v-for="item in computedTodos" v-bind:key="item.id">
+          <!-- 要素の情報 -->
+          <th>{{ item.id }}</th>
+          <td>{{ item.comment }}</td>
+          <td class="state">
+            <!-- 状態変更ボタンのモック -->
+            <button v-on:click="doChangeState(item)">
+              {{ labels[item.state] }}
+            </button>
+          </td>
+          <td class="button">
+            <!-- 削除ボタンのモック -->
+            <button v-on:click.ctrl="doRemove(item)">
+              削除
+            </button>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- [1] ここに <tr> で ToDo の要素を1行づつ繰り返し表示したい -->
+      </tbody>
+    </table>
+    <!-- 新規登録フォーム -->
+    <h2>新しい作業の追加</h2>
+    <form class="add-form" v-on:submit.prevent="doAdd">
+      <!-- コメント入力フォーム -->
+      コメント <input type="text" ref="comment">
+      <!-- 追加ボタンのモック -->
+      <button type="submit">追加</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+  // var STORAGE_KEY = 'todos-vuejs-demo'
+  // var todoStorage = {
+  //   fetch: function() {
+  //     var todos = JSON.parse(
+  //       localStorage.getItem(STORAGE_KEY) || '[]'
+  //     )
+  //     todos.forEach(function(todo, index) {
+  //       todo.id = index
+  //     })
+  //     todoStorage.uid = todos.length
+  //     return todos
+  //   },
+  //   save: function(todos) {
+  //     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  //   }
+  // },
+
+  var todoapp = new Vue ({
+    el: '#todoapp',
+    data: {
+      // 使用するデータ
+      todos:[],
+      options: [
+        { value: -1, label: 'すべて' },
+        { value: 0,  label: '作業中' },
+        { value: 1,  label: '完了' }
+      ],
+      // 選択している options の value を記憶するためのデータ
+      // 初期値を「-1」つまり「すべて」にする
+      current: -1
+    },
+    
+    methods: {
+      // 使用するメソッド
+      // ToDo 追加の処理
+      doAdd: function(event, value) {
+        // ref で名前を付けておいた要素を参照
+        var comment = this.$refs.comment
+        // 入力がなければ何もしないで return
+        if (!comment.value.length) {
+          return
+        }
+        // { 新しいID, コメント, 作業状態 }
+        // というオブジェクトを現在の todos リストへ push
+        // 作業状態「state」はデフォルト「作業中=0」で作成
+        this.todos.push({
+          id: todoStorage.uid++,
+          comment: comment.value,
+          state: 0
+        })
+        // フォーム要素を空にする
+        comment.value = ''
+      },
+      watch: {
+      // オプションを使う場合はオブジェクト形式にする
+        todos: {
+          // 引数はウォッチしているプロパティの変更後の値
+          handler: function(todos) {
+            todoStorage.save(todos)
+          },
+          // deep オプションでネストしているデータも監視できる
+          deep: true
+        }
+      },
+      // 状態変更の処理
+      doChangeState: function(item) {
+        item.state = item.state ? 0 : 1
+      },
+      // 削除の処理
+      doRemove: function(item) {
+        var index = this.todos.indexOf(item)
+        this.todos.splice(index, 1)
+      }
+    },
+
+    created() {
+      // インスタンス作成時に自動的に fetch() する
+      this.todos = todoStorage.fetch()
+    },
+
+    computed: {
+      computedTodos: function() {
+        // データ current が -1 ならすべて
+        // それ以外なら current と state が一致するものだけに絞り込む
+        return this.todos.filter(function(el) {
+          return this.current < 0 ? true : this.current === el.state
+        }, this)
+      },
+      labels() {
+        return this.options.reduce(function(a, b) {
+          return Object.assign(a, { [b.value]: b.label })
+        }, {})
+        // キーから見つけやすいように、次のように加工したデータを作成
+        // {0: '作業中', 1: '完了', -1: 'すべて'}
+      }
+    }
+  })
+
+
+</script>
+
+<style scoped>
+
+</style>
