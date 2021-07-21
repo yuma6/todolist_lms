300b8d7a12ec59cada2112ab3039d4575689d4d1
yumayumayuma <yumapuripuri@gmail.com>
2021-07-21 11:42:50 +0900

    配列とオブジェクト

--- array.js
+++ array.js
@@ -9,13 +9,13 @@ const object1 = {
 },
 
 // jsで「配列の配列」を書いてください。これを二重配列と言います。
-const array21 =['111','222','333'],
+const array21 =['111','222','333'],//配列
 const array22 =['aaa','bbb','ccc'],
 const array23 =['123','456','789'],
 const array2 = [array21,array22,array23],//配列の配列
 
 // jsで「オブジェクトを値に持つオブジェクト」を書いてください。
-const object21 = {property211:'value211ob',property212:'value212ob',property213:'value213ob'},
+const object21 = {property211:'value211ob',property212:'value212ob',property213:'value213ob'},//オブジェクト
 const object22 = {property221:'value221ob',property222:'value222ob',property223:'value223ob'},
 const object23 = {property231:'value231ob',property232:'value232ob',property233:'value233ob'},
 const object2 = {property21:object21,property22:object22,property23:object23},//オブジェクトを値に持つオブジェクト
@@ -27,7 +27,7 @@ const object33 = {property31:array21,property32:array22,property33:array23},
 const array3 = [object31,object32,object33],//配列を値に持つオブジェクトの配列
 
 // jsで「オブジェクトの配列を値に持つオブジェクト」を書いてください。
-const array41 =[array21,array22,array23],//オブジェクトの配列
-const array42 =[array21,array22,array23],
-const array43 =[array21,array22,array23],
+const array41 =[object21,object22,object23],//オブジェクトの配列
+const array42 =[object21,object22,object23],
+const array43 =[object21,object22,object23],
 const object4 ={property41:array41,property42:array42,property43:array43}//オブジェクトの配列を値に持つオブジェクト
